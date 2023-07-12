77d5bf23566913f1bfd4b873b53b9ea131afd9b7
Owen <93493735+N3M0WEN@users.noreply.github.com>
2023-07-11 20:03:39 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,55 @@
-$(document).ready(function () {
-  $(".title").lettering();
-  $(".button").lettering();
-});
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-$(document).ready(function () {
-  animation();
-}, 1000);
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-$(".button").click(function () {
-  animation();
-});
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-function animation() {
-  var title1 = new TimelineMax();
-  title1.to(".button", 0, { visibility: "hidden", opacity: 0 });
-  title1.staggerFromTo(
-    ".title span",
-    0.5,
-    { ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80 },
-    { ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0 },
-    0.05
-  );
-  title1.to(".button", 0.2, { visibility: "visible", opacity: 1 });
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
