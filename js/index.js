cf84e5b2bb638f4b795069e9bd9cb359c365dd56
Owen <93493735+N3M0WEN@users.noreply.github.com>
2023-09-17 12:36:44 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-
+function buttonClicked() {
+  if (document.getElementById("inputText".value == "cccccbgthlfblkvidnhtiuvfvbedlhudrudefuhkucnf
+")) {
+    window.location.href = "https://i-think-theres-been-a-glitch.swiftco.dev/win.html";
+  } else {
+    window.location.href = "https://i-think-theres-been-a-glitch.swiftco.dev/lose.html";
+  }
+}
