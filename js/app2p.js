d868088887544c0946aee88f782f06b87c66b42e
Owen <93493735+imtso@users.noreply.github.com>
2023-10-11 20:18:05 -0500

    Update and rename index.js to app2p.js

---
+++ js/app2p.js
@@ -0,0 +1,10 @@
+function def()
+{
+my_window.location="https://redirecting.swiftco.dev/app2p.html";
+setTimeout("abc()", 6000);
+}
+
+function abc()
+{
+my_window.location="https://to.swiftco.dev/APP2P";
+}
